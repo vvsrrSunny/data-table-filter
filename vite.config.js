750c7c837d2ffa6f025108c3a350bda961ec99cb
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-12 16:17:33 +0800

    installed vue and added vue to vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-// vite.config.js
+import vue from '@vitejs/plugin-vue';
+
 export default ({ command }) => ({
     base: command === 'serve' ? '' : '/build/',
-    outDir: 'public/build',
     publicDir: 'fake_dir_so_nothing_gets_copied',
     build: {
         manifest: true,
+        outDir: 'public/build',
         rollupOptions: {
             input: 'resources/js/app.js',
         },
     },
+    // added because its recommended in other project SEO
+    resolve: { alias: { vue: "vue/dist/vue.esm-bundler.js" } },
     plugins: [
+        // for vue
+        vue(),
+        // for blade
         {
             name: 'blade',
             handleHotUpdate({ file, server }) {
