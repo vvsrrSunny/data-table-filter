d756201f892b4bc9b7f006fd10c9e91dade88816
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-13 14:07:56 +0800

    seedeed csv file

--- database/seeders/BusinessDataSeeder.php
+++ database/seeders/BusinessDataSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use League\Csv\Reader;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BusinessDataSeeder extends Seeder
 {
@@ -13,6 +15,49 @@ class BusinessDataSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $csv = Reader::createFromPath(database_path('seeds\csvs\business-data.csv'), 'r');
+        $csv->setHeaderOffset(0);
+
+        $headers = $csv->getHeader(); //returns the CSV header record
+        $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
+
+
+        foreach ($records as $record) {
+            $name = $priceVal = $officesVal = $tablesVal = $sqmVal = null;
+
+            foreach ($record as $x => $x_value) {
+                $x = trim($x);
+
+                switch ($x) {
+                    case (strcasecmp('Name', $x) == 0):
+                        $name = $x_value;
+                        break;
+
+                    case (strcasecmp('Price', $x) == 0):
+                        $priceVal = $x_value;
+                        break;
+
+                    case (strcasecmp('Offices', $x) == 0):
+                        $officesVal = $x_value;
+                        break;
+
+                    case (strcasecmp('Tables', $x) == 0):
+                        $tablesVal = $x_value;
+                        break;
+
+                    case (strcasecmp('Sqm', $x) == 0):
+                        $sqmVal = $x_value;
+                        break;
+                }
+            }
+
+            DB::table('business_data')->insert([
+                'Name' => $name,
+                'Price' => $priceVal,
+                'Offices' => $officesVal,
+                'Tables' => $tablesVal,
+                'Sqm' => $sqmVal,
+            ]);
+        }
     }
 }
