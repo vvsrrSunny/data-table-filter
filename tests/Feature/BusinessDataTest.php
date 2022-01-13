ed1ebfe15610c5475fa08745d956d116d925ce29
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-13 19:58:35 +0800

    following TDD, added a failed test case

--- tests/Feature/BusinessDataTest.php
+++ tests/Feature/BusinessDataTest.php
@@ -2,20 +2,41 @@
 
 use App\Models\BusinessData;
 
-it('can view the business offices dashboard', function () {
-    test()->get(route('business.index'))
+test('can view the business offices dashboard', function () {
+    $this->get(route('business.index'))
         ->assertStatus(200)
         ->assertViewIs('dashboard');
 });
 
-it('can get a listing of offices', function () {
+test('can get a listing of offices', function () {
     BusinessData::factory()->count(7)->create();
 
-    test()
-        ->getJson(route('business.index'))
+    $this->getJson(route('business.index'))
         ->assertOk()
         ->assertJson([
             'total' => 7,
             'per_page' => 5,
+        ]);
+});
+
+test('can filter the business data table by name', function () {
+    BusinessData::factory()->count(5)->create();
+
+    BusinessData::find(1)->update(['name' => 'Perth Northbridge']);
+
+    // Can filter results
+    $this
+        ->getJson(route('business.index', ['search' => 'Perth Northbridge']))
+        ->assertOk()
+        ->assertJson([
+            'total' => 1,
+        ]);
+
+    // Works on a partial search
+    $this
+        ->getJson(route('business.index', ['search' => 'Perth']))
+        ->assertOk()
+        ->assertJson([
+            'total' => 1,
         ]);
 });
