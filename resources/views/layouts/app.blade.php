8a7e3636041e1f8b4b5a153e7f5ba9e40444c51d
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-17 20:09:30 +0800

    added page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,17 +13,48 @@
 
 </head>
 
-<body id="app" class="font-sans antialiased">
+<body class="font-sans antialiased">
+    <div>
+      <div class="bg-gray-800 pb-32">
+        <nav class="bg-gray-800">
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="border-b border-gray-700">
+              <div class="flex items-center justify-between h-16 px-4 sm:px-0">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0">
+                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Sunny Logo">
+                  </div>
 
-    <div class="min-h-screen bg-gray-100">
+                  <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                      <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
 
+        <header class="py-10">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold text-white">
+              Business Data Table
+            </h1>
+          </div>
+        </header>
+      </div>
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+      <main class="-mt-32">
+        <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+          <div id="app" class="bg-white rounded-lg shadow px-2 py-6 sm:px-6"> {{$slot}}</div>
+        </div>
+      </main>
     </div>
+
+    @stack('scripts')
+
     {{ vite_assets() }}
-</body>
+  </body>
 
 </html>
