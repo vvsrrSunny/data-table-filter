74318ccbd114685a9cff4e68e8cb01fadab91dee
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-27 14:13:37 +0800

    adjusted the UI for search

--- resources/js/components/Filters.vue
+++ resources/js/components/Filters.vue
@@ -1,23 +1,73 @@
 <template>
-  <div
-    class="
-      pb-3
-      flex flex-col
-      space-y-2
-      md:flex-row md:items-end md:space-x-2 md:space-y-0
-    "
-  >
-    <div>
-      Offices
-      <the-input
-        v-model="offices"
-        type="number"
-        id="offices"
-        placeholder="Offices"
-      />
+  <div class="pb-3 flex flex-row justify-between items-end">
+    <div
+      class="
+        flex flex-col
+        space-y-2
+        md:flex-row md:items-end md:space-x-2 md:space-y-0
+      "
+    >
+      <div>
+        Offices
+        <the-input
+          v-model="offices"
+          type="number"
+          id="offices"
+          placeholder="Offices"
+        />
+      </div>
+      <div>
+        Tables
+        <the-input
+          v-model="tables"
+          type="number"
+          id="tables"
+          placeholder="Tables"
+          class="w-40"
+        />
+      </div>
+
+      <div>
+        Price
+        <div class="flex flex-row w-40">
+          <the-input
+            v-model="price.from"
+            type="number"
+            id="price-from"
+            placeholder="from"
+            rounded="rounded-l-md"
+          />
+          <the-input
+            v-model="price.to"
+            type="number"
+            id="price-to"
+            placeholder="to"
+            rounded="rounded-r-md"
+          />
+        </div>
+      </div>
+
+      <div>
+        No of Square Meters
+        <div class="flex flex-row w-40">
+          <the-input
+            v-model="square_meters.from"
+            type="number"
+            id="square-meters-from"
+            placeholder="from"
+            rounded="rounded-l-md"
+          />
+          <the-input
+            v-model="square_meters.to"
+            type="number"
+            id="square-meters-to"
+            placeholder="to"
+            rounded="rounded-r-md"
+          />
+        </div>
+      </div>
     </div>
     <div>
-      Tables
       <the-input
         v-model="tables"
         type="number"
@@ -25,46 +75,6 @@
         placeholder="Tables"
         class="w-40"
       />
-    </div>
-
-    <div>
-      Price
-      <div class="flex flex-row w-40">
-        <the-input
-          v-model="price.from"
-          type="number"
-          id="price-from"
-          placeholder="from"
-          rounded="rounded-l-md"
-        />
-        <the-input
-          v-model="price.to"
-          type="number"
-          id="price-to"
-          placeholder="to"
-          rounded="rounded-r-md"
-        />
-      </div>
-    </div>
-
-    <div>
-      No of Square Meters
-      <div class="flex flex-row w-40">
-        <the-input
-          v-model="square_meters.from"
-          type="number"
-          id="square-meters-from"
-          placeholder="from"
-          rounded="rounded-l-md"
-        />
-        <the-input
-          v-model="square_meters.to"
-          type="number"
-          id="square-meters-to"
-          placeholder="to"
-          rounded="rounded-r-md"
-        />
-      </div>
     </div>
   </div>
 </template>
