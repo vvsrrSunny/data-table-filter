6ac287fd01fed75217c68a8d5f30627f4c7e574a
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-12-04 23:13:28 +0800

    tweaked the route to get the result

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\http\controllers\BusinessDataController')->name('business.index');
+// Route::get('/', 'App\http\controllers\BusinessDataController')->name('business.index');
+Route::get('/', 'BusinessDataController')->name('business.index');
